613e63ae30b2de366c499ff5009abfcdf3e2aa71
natasharavinand <natasharavinand@gmail.com>
2021-01-08 13:24:38 -0800

    update env vars

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(session({
-  secret: "84778943783hbfjhfsdhbfjs73",
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: false
 }));
